0891ad9507036815902ea2806c38770544ec7bca
Venkat <venkatesh.devulaeplly@ierl.in>
2024-10-09 16:54:23 +0530

    cors enabled

--- apigateway/src/main.ts
+++ apigateway/src/main.ts
@@ -3,7 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors()
+  app.enableCors({
+    origin: ['http://localhost:8080/'],
+    credentials: true
+  })
   await app.listen(3000);
   console.log(`**** Application is running on : ${await app.getUrl()} ****`)
 }
